767e9cb951ac7810a650507a8d43c81062adfee5
brentlien <brentlien@msn.com>
2025-07-29 10:14:46 -0500

    Fix proxy connection error

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/webhook': {
-        target: 'https://localhost:5678',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook-test/122780b8-983f-4b2f-b717-1ca45a90b417')
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
