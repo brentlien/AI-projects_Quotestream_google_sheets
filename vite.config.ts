7b6b5a48532f42a3b068b7fc11d66ebc7fbc049d
brentlien <brentlien@msn.com>
2025-07-29 10:09:52 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/webhook': {
-        target: 'https://blien.app.n8n.cloud',
+        target: 'https://localhost:5678',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook-test/122780b8-983f-4b2f-b717-1ca45a90b417')
       }
